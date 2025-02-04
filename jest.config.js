c256527c473d5972be626eaa9c8332ac3f3211d0
pulagumsaii <155514623+pulagumsaii@users.noreply.github.com>
2025-02-04 20:18:36 +0530

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-    //... (rest of the config remains the same)
-    preset: 'ts-jest',
-    roots: ['<rootDir>'],
-    collectCoverage: true,
-    coverageReporters: ['json', 'lcov', 'clover'],
-  };
